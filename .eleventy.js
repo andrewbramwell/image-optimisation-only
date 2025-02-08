1a5a33e15d153abf6d1d12ee5bf39445d21c7370
andrewbramwell <andy@voucherstore.io>
2025-02-08 09:31:05 +0000

    first commit

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,10 @@
 const pluginEleventyNavigation = require("@11ty/eleventy-navigation");
 const pluginMinifier = require("@sherby/eleventy-plugin-files-minifier");
 const pluginSitemap = require("@quasibit/eleventy-plugin-sitemap");
+// eleventy.js
+
+const eleventyPluginSharpImages = require("@codestitchofficial/eleventy-plugin-sharp-images");
+
 
 // Configs
 const configCss = require("./src/config/css");
@@ -41,7 +45,10 @@ module.exports = function (eleventyConfig) {
     /**=====================================================================
                   PLUGINS - Adds additional eleventy functionality 
     =======================================================================*/
-    /** https://www.11ty.dev/docs/plugins/ */
+    eleventyConfig.addPlugin(eleventyPluginSharpImages, {
+        urlPath: "/assets/images",
+        outputDir: "public/assets/images",
+    });
 
     /**
      *  ELEVENTY NAVIGATION
